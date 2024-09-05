9188bc9ea0e6a6a721cf812978616cb25d766a3a
sambuchito <139599455+sambuchito@users.noreply.github.com>
2024-09-04 22:53:02 -0300

    Agregado de PacienteServiceTest

--- src/main/java/com/rojo/ClinicaOdontologia/controller/TurnoController.java
+++ src/main/java/com/rojo/ClinicaOdontologia/controller/TurnoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/turnos")
+@RequestMapping("/turno")
 public class TurnoController {
     private ITurnoService turnoService;
 
