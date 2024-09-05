34c1815d252e399ba6cde3b887e7b8a272b74bea
sambuchito <139599455+sambuchito@users.noreply.github.com>
2024-09-04 23:47:52 -0300

    Agregado de OdontologoServiceTest + TurnoServiceTest

--- src/test/java/com/rojo/ClinicaOdontologia/OdontologoServiceTest.java
+++ src/test/java/com/rojo/ClinicaOdontologia/OdontologoServiceTest.java
@@ -1,50 +1,74 @@
 package com.rojo.ClinicaOdontologia;
 
+import com.rojo.ClinicaOdontologia.entity.Domicilio;
 import com.rojo.ClinicaOdontologia.entity.Odontologo;
+import com.rojo.ClinicaOdontologia.entity.Paciente;
+import com.rojo.ClinicaOdontologia.service.impl.PacienteService;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.rojo.ClinicaOdontologia.service.impl.OdontologoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
 public class OdontologoServiceTest {
-//    static final Logger logger = LoggerFactory.getLogger(OdontologoServiceTest.class);
-//    OdontologoService odontologoService = new OdontologoService(new DaoH2Odontologo());
-//
-//    @BeforeAll
-//    static void crearTablas(){
-//        Connection connection = null;
-//        try {
-//            Class.forName("org.h2.Driver");
-//            connection = DriverManager.getConnection("jdbc:h2:./ClinicaOdontologica;INIT=RUNSCRIPT FROM 'CrearTablasBD.sql'", "sa", "sa");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            logger.error(e.getMessage());
-//        } finally {
-//            try {
-//                connection.close();
-//            } catch (SQLException e) {
-//                logger.error(e.getMessage());
-//            }
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("Testear listado de todos los odontologos")
-//    void ListadoDeTodos(){
-//        //dado
-//        List<Odontologo> listaOdontologos;
-//        //cuando
-//        listaOdontologos=odontologoService.listarTodos();
-//        //entonces
-//        assertFalse(listaOdontologos.isEmpty());
-//    }
+    static final Logger logger = LoggerFactory.getLogger(OdontologoServiceTest.class);
+    @Autowired
+    OdontologoService odontologoService;
+    Odontologo odontologo;
+    Odontologo odontologoDesdeDb;
+
+    @BeforeEach
+    void cargarDatos(){
+        odontologo = new Odontologo();
+        odontologo.setNumeroMatricula(1512);
+        odontologo.setApellido("Castra");
+        odontologo.setNombre("Mario");
+        odontologoDesdeDb = odontologoService.guardarOdontologo(odontologo);
+    }
+
+    @Test
+    @DisplayName("Testear que un odontologo fue cargado correctamente")
+    void caso1(){
+
+        assertNotNull(odontologoDesdeDb.getId());
+    }
+
+    @Test
+    @DisplayName("Testear que un odontologo pueda acceder por id")
+    void caso2(){
+        //Dado
+        Integer id = odontologoDesdeDb.getId();
+        //cuando
+        Odontologo odontologoRecuperado = odontologoService.buscarPorId(id).get();
+        // entonces
+        assertEquals(id, odontologoRecuperado.getId());
+    }
+
+    @Test
+    @DisplayName("Testear listado de todos los odontologos")
+    void ListadoDeTodos(){
+        //dado
+        List<Odontologo> listaOdontologos;
+        //cuando
+        listaOdontologos=odontologoService.buscarTodos();
+        //entonces
+        assertFalse(listaOdontologos.isEmpty());
+    }
 }
---
+++ src/test/java/com/rojo/ClinicaOdontologia/TurnoServiceTest.java
@@ -0,0 +1,70 @@
+package com.rojo.ClinicaOdontologia;
+
+import com.rojo.ClinicaOdontologia.dto.request.TurnoRequestDto;
+import com.rojo.ClinicaOdontologia.dto.response.TurnoResponseDto;
+import com.rojo.ClinicaOdontologia.entity.Odontologo;
+import com.rojo.ClinicaOdontologia.entity.Turno;
+import com.rojo.ClinicaOdontologia.service.impl.OdontologoService;
+import com.rojo.ClinicaOdontologia.service.impl.TurnoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
+public class TurnoServiceTest {
+    static final Logger logger = LoggerFactory.getLogger(TurnoServiceTest.class);
+    @Autowired
+    TurnoService turnoService;
+    TurnoRequestDto turno;
+    TurnoResponseDto turnoDesdeDb;
+
+    @BeforeEach
+    void cargarDatos(){
+        turno = new TurnoRequestDto();
+        turno.setPaciente_id(1);
+        turno.setOdontologo_id(1);
+        turno.setFecha("2024-11-11");
+        turnoDesdeDb = turnoService.guardarTurno(turno);
+    }
+
+    @Test
+    @DisplayName("Testear que un turno fue cargado correctamente")
+    void caso1(){
+
+        assertNotNull(turnoDesdeDb.getId());
+    }
+
+    @Test
+    @DisplayName("Testear que un turno pueda acceder por id")
+    void caso2(){
+        //Dado
+        Integer id = turnoDesdeDb.getId();
+        //cuando
+        TurnoResponseDto turnoRecuperado = turnoService.buscarPorId(id).get();
+        // entonces
+        assertEquals(id, turnoRecuperado.getId());
+    }
+
+//    @Test
+//    @DisplayName("Testear listado de todos los turnos")
+//    void ListadoDeTodos(){
+//        //dado
+//        List<turno> listaTurno;
+//        //cuando
+//        listaTurnos=odontologoService.buscarTodos();
+//        //entonces
+//        assertFalse(listaOdontologos.isEmpty());
+//    }
+}
